1ce9ae3b6b2f7c100ccbd2abc86266e2594ce265
Matheus Felipe <matheusfelipetp@outlook.com>
2022-12-12 17:22:29 -0300

    feat: add structure project

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-import "reflect-metadata"
-import express from "express"
+import 'reflect-metadata';
+import express from 'express';
+import { router } from './routers/routes';
 
+const app = express();
+app.use(express.json());
+app.use(router);
 
-
-const app = express()
-app.use(express.json())
-
-
-export default app
+export default app;
